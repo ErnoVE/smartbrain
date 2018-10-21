89dba396c311237c231a853d758a88f8b38b2e00
Erno <erno.ve@gmail.com>
2018-10-21 15:34:54 +0300

    Added Rank.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
-import './App.css';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import './App.css';
+
+const ParticlesOptions = {
+    particles: {
+      number: {
+        value: 50,
+        density: {
+        enable: true,
+        value_area: 800
+      }
+      } 
+    }
+  }
 
 class App extends Component {
   render() {
     return (  
       <div className="App">
+        <Particles className="particles"
+        params={ParticlesOptions}
+        />
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
+
         {/*<FaceRecognition /> */}
       </div>
     );
